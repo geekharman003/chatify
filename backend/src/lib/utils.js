2c1120db9b3e588068910ef4255798f8334e8014
geekharman003 <harmansadhra2003@gmail.com>
2026-02-09 16:55:28 +0530

    welcome email functionality implemented

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  const { JWT_SECRET } = process.env;
+  const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) throw new Error("JWT_SECRET is not set");
 
   const token = jwt.sign({ userId }, JWT_SECRET, {
@@ -12,6 +13,6 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV === "development" ? false : true,
+    secure: ENV.NODE_ENV === "development" ? false : true,
   });
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
-
-dotenv.config();
+import { ENV } from "./lib/env.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
 
