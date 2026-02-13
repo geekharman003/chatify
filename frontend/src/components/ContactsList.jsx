ee19707e08f4de42c37f1995870754db97b9656a
geekharman003 <harmansadhra2003@gmail.com>
2026-02-13 23:53:57 +0530

    created the sidebar

--- frontend/src/components/ContactsList.jsx
+++ frontend/src/components/ContactsList.jsx
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
+
+
+
 
 function ContactsList() {
+  const {contacts,getAllContacts,isUsersLoading,setSelectedUser} = useChatStore();
+
+
+  useEffect(()=> {
+    getAllContacts();
+  },[getAllContacts])
+
+  if(isUsersLoading) return <UsersLoadingSkeleton />
+
+
   return (
-    <div>ContactsList</div>
+    contacts.map((contact) => (
+        <div
+          key={contact._id}
+          className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+          onClick={() => setSelectedUser(contact)}
+        >
+          <div className="flex items-center gap-3">
+            <div className={`avatar online`}>
+              <div className="size-12 rounded-full">
+                <img src={contact.profilePic || "/avatar.png"} alt={contact.fullName} />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">{contact.fullName}</h4>
+          </div>
+        </div>
+    ))
   )
 }
 
